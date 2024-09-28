730e0306a1346ea7a86d0237bb024f4dab17e758
LoGin <longjin@DragonOS.org>
2024-09-29 02:11:37 +0800

    fix: 修正对tty读入的字符的处理,使其与Linux一致 (#46)
    Signed-off-by: longjin <longjin@dragonos.org>

--- src/keycode.rs
+++ src/keycode.rs
@@ -10,12 +10,17 @@ pub enum SpecialKeycode {
     BackSpace = b'\x08',
     Tab = b'\t',
 
-    FunctionKeyPrefix = 0xE0,
+    ESC = 0x1B,
     PauseBreak = 0xE1,
 }
 
-#[repr(u8)]
-#[derive(Debug, FromPrimitive, TryFromPrimitive, ToPrimitive, PartialEq, Eq, Clone)]
+impl Into<u8> for SpecialKeycode {
+    fn into(self) -> u8 {
+        self as u8
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 #[allow(dead_code)]
 pub enum FunctionKeySuffix {
     Up = 0x48,
@@ -27,8 +32,34 @@ pub enum FunctionKeySuffix {
     End = 0x4F,
 }
 
-impl Into<u8> for SpecialKeycode {
-    fn into(self) -> u8 {
-        self as u8
+impl FunctionKeySuffix {
+    pub const SUFFIX_0: u8 = 0x5b;
+    pub fn bytes(self) -> &'static [u8] {
+        match self {
+            FunctionKeySuffix::Up => &[0x5b, 0x41],
+            FunctionKeySuffix::Down => &[0x5b, 0x42],
+            FunctionKeySuffix::Left => &[0x5b, 0x44],
+            FunctionKeySuffix::Right => &[0x5b, 0x43],
+            FunctionKeySuffix::Home => &[0x5b, 0x48],
+            FunctionKeySuffix::End => &[0x5b, 0x46],
+        }
+    }
+
+    pub fn try_from(value: &[u8]) -> Option<Self> {
+        match value {
+            [0x5b, 0x41] => Some(FunctionKeySuffix::Up),
+            [0x5b, 0x42] => Some(FunctionKeySuffix::Down),
+            [0x5b, 0x44] => Some(FunctionKeySuffix::Left),
+            [0x5b, 0x43] => Some(FunctionKeySuffix::Right),
+            [0x5b, 0x48] => Some(FunctionKeySuffix::Home),
+            [0x5b, 0x46] => Some(FunctionKeySuffix::End),
+            _ => None,
+        }
+    }
+}
+
+impl Into<&[u8]> for FunctionKeySuffix {
+    fn into(self) -> &'static [u8] {
+        self.bytes()
     }
 }
